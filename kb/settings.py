41b5f57d16ba872434d47ea722f67a0e77c46be7
Christo Crampton <info@38.co.za>
2022-01-17 03:02:22 +0200

    Add allowed_hosts

--- kb/settings.py
+++ kb/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "agkb.herokuapp.com",
+    "help.appointmentguru.co",
+]
 
 
 # Application definition
