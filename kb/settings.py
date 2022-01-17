6616531a48f4b9c99cd71403af95ebe4eb275e54
Christo Crampton <info@38.co.za>
2022-01-17 02:59:57 +0200

    extract SECRET_KEY

--- kb/settings.py
+++ kb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "h_5@)#we0agnh%q5bpn3-n65x5(ojil8sq#j9)xno1_3gi-n3)"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
